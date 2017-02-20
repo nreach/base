0ff24cb8d931b4edc3d0780be3c2ed83eedc149d
Johannes Goslar <johannes.goslar@dkd.de>
2017-02-20 17:11:47 +0100

    In the beginning was the deed!

---
+++ src/DataSourceDelegateInterface.php
@@ -0,0 +1,6 @@
+<?php
+namespace Nreach\Base;
+
+interface DataSourceDelegateInterface
+{
+}
---
+++ src/Service.php
@@ -0,0 +1,44 @@
+<?php
+namespace Nreach\Base;
+use \GuzzleHttp\Client;
+
+class Service
+{
+    private $client = NULL;
+    private $remoteHost = NULL;
+    private $keys = NULL;
+    private $delegate = NULL;
+
+    private $mappings = [
+        'imageanalyzer' => 'imageanalyzer',
+        'textanalyzer' => 'textanalyzer',
+        'chattoken' => 'bot'
+    ];
+
+    function __construct(string $remoteHost, array $keys, DataSourceDelegateInterface $delegate) {
+        $this->client = new \GuzzleHttp\Client();
+        $this->remoteHost = $remoteHost;
+        $this->keys = $keys;
+        $this->delegate = $delegate;
+    }
+
+    private function remote(string $method, array $arguments = [], array $body = null) {
+        $api = $this->mappings[$method];
+        $url = $this->remoteHost . '' . $api;
+        $arguments = [
+            'headers' => [
+                'Ocp-Apim-Subscription-Key' => $this->keys[$api]
+            ],
+            'query' => $arguments,
+            'json' => $body
+        ];
+
+        $result = $this->client->post($url, $arguments);
+        return $result->getBody();
+    }
+
+    public function call($method, $arguments, $body)
+    {
+        return $this->remote($method, $arguments, $body);
+    }
+}
